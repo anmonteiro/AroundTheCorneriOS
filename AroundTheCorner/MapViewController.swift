68c3c01fe8fdc5089ff5d505d777345f9fe637e3
António Nuno Monteiro <anmonteiro@gmail.com>
2015-12-17 17:54:24 +0100

    map filters wit

--- AroundTheCorner/MapViewController.swift
+++ AroundTheCorner/MapViewController.swift
@@ -10,83 +10,96 @@ import UIKit
 import GoogleMaps
 
 class MapViewController: UIViewController, CLLocationManagerDelegate {
-    var locationManager = CLLocationManager()
-    var didFindMyLocation = false
+  var locationManager = CLLocationManager()
+  var didFindMyLocation = false
+  var filters : [String]? = nil
+  
+  @IBOutlet weak var filterBtn: UIBarButtonItem!
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    locationManager.delegate = self
+    locationManager.requestWhenInUseAuthorization()
+    
+    //        let camera = GMSCameraPosition.cameraWithLatitude(-33.86,
+    //            longitude: 151.20, zoom: 6)
+    let mapView = GMSMapView(frame: CGRectZero)
+    
+    mapView.settings.compassButton = true;
+    //mapView.myLocationEnabled = true
+    mapView.addObserver(self, forKeyPath: "myLocation", options: NSKeyValueObservingOptions.New, context: nil)
+    mapView.padding = UIEdgeInsetsMake(64, 0, 64, 0)
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
-        
-        locationManager.delegate = self
-        locationManager.requestWhenInUseAuthorization()
-        
-//        let camera = GMSCameraPosition.cameraWithLatitude(-33.86,
-//            longitude: 151.20, zoom: 6)
-        let mapView = GMSMapView(frame: CGRectZero)
-        
-        mapView.settings.compassButton = true;
-        //mapView.myLocationEnabled = true
-        mapView.addObserver(self, forKeyPath: "myLocation", options: NSKeyValueObservingOptions.New, context: nil)
-        self.view = mapView
-        
-        /*
-        let marker = GMSMarker()
-        marker.position = CLLocationCoordinate2DMake(-33.86, 151.20)
-        marker.title = "Sydney"
-        marker.snippet = "Australia"
-        marker.map = mapView
-        */
-    }
+    self.view = mapView
     
-    override func viewWillAppear(animated: Bool) {
-        super.viewWillAppear(animated)
-//        print("view will appear")
-//        if let mapView = self.view as? GMSMapView {
-//            print("view is mapview")
-//            if let currLoc = mapView.myLocation {
-//                print("current location: \(currLoc)")
-//                //mapView.animateToLocation(<#T##location: CLLocationCoordinate2D##CLLocationCoordinate2D#>)
-//            }
-//            else {
-//                print("still waiting for user's location or not enabled.")
-//            }
-//        }
-        
-    }
+    self.filterBtn.target = self
+    self.filterBtn.action = "toFilterView:"
     
-    override func viewDidAppear(animated: Bool) {
-        if let mapView = self.view as? GMSMapView {
-            mapView.padding = UIEdgeInsetsMake(64, 0, 64, 0)
-        }
+    // TODO: call Places API & present the markers according to the active filters
+    /*
+    let marker = GMSMarker()
+    marker.position = CLLocationCoordinate2DMake(-33.86, 151.20)
+    marker.title = "Sydney"
+    marker.snippet = "Australia"
+    marker.map = mapView
+    */
+  }
+  
+  func toFilterView(sender: AnyObject) {
+    self.performSegueWithIdentifier("toFilterViewSegue", sender: self)
+  }
+  
+  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    if segue.identifier == "toFilterViewSegue" {
+      let viewController = segue.destinationViewController as! FilterViewController
+      viewController.mapViewControllerInstance = self
     }
-    
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-    func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
-        if status == CLAuthorizationStatus.AuthorizedWhenInUse {
-            if let mapView = self.view as? GMSMapView {
-                mapView.myLocationEnabled = true
-            }
-        }
-    }
-    
-    override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
-        if !didFindMyLocation {
-            let myLocation: CLLocation = change![NSKeyValueChangeNewKey] as! CLLocation
-            if let mapView = self.view as? GMSMapView {
-                let camPos = GMSCameraPosition.cameraWithTarget(myLocation.coordinate, zoom: 16.0)
-                mapView.animateToCameraPosition(camPos)
-//                mapView.camera = 
-                
-                mapView.settings.myLocationButton = true
-            }
-            
-            didFindMyLocation = true
-        }
-    }
+  }
+  
+  override func viewWillAppear(animated: Bool) {
+    super.viewWillAppear(animated)
+    //        print("view will appear")
+    //        if let mapView = self.view as? GMSMapView {
+    //            print("view is mapview")
+    //            if let currLoc = mapView.myLocation {
+    //                print("current location: \(currLoc)")
+    //                //mapView.animateToLocation(<#T##location: CLLocationCoordinate2D##CLLocationCoordinate2D#>)
+    //            }
+    //            else {
+    //                print("still waiting for user's location or not enabled.")
+    //            }
+    //        }
+  }
 
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+  
+  func locationManager(manager: CLLocationManager, didChangeAuthorizationStatus status: CLAuthorizationStatus) {
+    if status == CLAuthorizationStatus.AuthorizedWhenInUse {
+      if let mapView = self.view as? GMSMapView {
+        mapView.myLocationEnabled = true
+      }
+    }
+  }
+  
+  override func observeValueForKeyPath(keyPath: String?, ofObject object: AnyObject?, change: [String : AnyObject]?, context: UnsafeMutablePointer<Void>) {
+    if !didFindMyLocation {
+      let myLocation: CLLocation = change![NSKeyValueChangeNewKey] as! CLLocation
+      if let mapView = self.view as? GMSMapView {
+        let camPos = GMSCameraPosition.cameraWithTarget(myLocation.coordinate, zoom: 16.0)
+        mapView.animateToCameraPosition(camPos)
+        //                mapView.camera =
+        
+        mapView.settings.myLocationButton = true
+      }
+      
+      didFindMyLocation = true
+    }
+  }
+  
 }
 
--- FilterViewController.swift
+++ FilterViewController.swift
@@ -9,78 +9,130 @@
 import UIKit
 
 class FilterViewController : UIViewController, UITableViewDelegate, UITableViewDataSource {
+  var mapViewControllerInstance : MapViewController? = nil
+  
+  @IBOutlet weak var navigationBar: UINavigationBar!
+  @IBOutlet weak var filterTableView: UITableView!
+  @IBOutlet weak var applyBtn: UIBarButtonItem!
+  
+  /****************************
+   **                        **
+   ** ViewController Methods **
+   **                        **
+   ****************************/
+  
+  override func viewDidLoad() {
+    super.viewDidLoad()
     
-    @IBOutlet weak var navigationBar: UINavigationBar!
-    @IBOutlet weak var filterTableView: UITableView!
+    ATCUtils.removeNavBarBgAndShadow(self.navigationBar)
     
-    /****************************
-     **                        **
-     ** ViewController Methods **
-     **                        **
-     ****************************/
+    // associate the table view with this view controller
+    self.filterTableView.delegate = self;
+    self.filterTableView.dataSource = self;
+    self.filterTableView.allowsMultipleSelection = true;
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        ATCUtils.removeNavBarBgAndShadow(self.navigationBar)
-        
-        // associate the table view with this view controller
-        self.filterTableView.delegate = self;
-        self.filterTableView.dataSource = self;
-        
-        // TODO: set up the "Apply" button's action
+    applyBtn.target = self
+    applyBtn.action = "applyFilter:"
+  }
+  
+  func applyFilter(sender: UIBarButtonItem) {
+    var places = [String]()
+    if let selectedRows = self.filterTableView.indexPathsForSelectedRows {
+      let filterKeys = Array(self.placeTypes.keys)
+      for ip in selectedRows {
+        let idx = ip.row
+        places.append(self.placeTypes[filterKeys[idx]]!)
+      }
     }
-    
-    /*************************
-     **                     **
-     ** UITableView Methods **
-     **                     **
-     *************************/
-    
-    var placeTypes = ["Bar", "Bakery", "Cafe", "Casino", "Convenience Store", "Grocery Store / Supermarket",
-                      "Liquor Store", "Night Club", "Restaurant"]
-    
-    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
-        return 2;
+    else {
+      // TODO: what to do for empty selection?
     }
-    
-    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        // the following assumes there are only 2 sections
-        return section == 0 ? 1 : placeTypes.count
+    self.performSegueWithIdentifier("applyFiltersSegue", sender: places)
+  }
+  
+  
+  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    if segue.identifier == "applyFiltersSegue" {
+      //we know that sender is an array here.
+      let filters = sender as! Array<String>;
+      mapViewControllerInstance?.filters = filters
     }
-    
-    func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        // the following assumes there are only 2 sections
-        return section == 0 ? "Search radius" : "Place type"
-    }
-    
-    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        // the following assumes there are only 2 sections
-        let cell = tableView.dequeueReusableCellWithIdentifier("tableCell", forIndexPath: indexPath)
-        
-        if indexPath.section == 0 {
-            // "search radius" slider section
-            let slider = UISlider()
-            
-            // TODO: fix images showing as black rectangle
-            cell.contentView.addSubview(slider)
-            let imgSize = CGSize(width: slider.bounds.size.height, height: slider.bounds.size.height)
-            let minImage = ATCUtils.scaleUIImageToSize(UIImage(named: "slider-minus")!, size: imgSize)
-            let maxImage = ATCUtils.scaleUIImageToSize(UIImage(named: "slider-plus")!, size: imgSize)
-            slider.minimumValueImage = minImage
-            slider.maximumValueImage = maxImage
+  }
 
-            slider.bounds = CGRectMake(0, 0, cell.contentView.bounds.size.width - 10, slider.bounds.size.height)
-            slider.center = CGPointMake(CGRectGetMidX(cell.contentView.bounds), CGRectGetMidY(cell.contentView.bounds))
-            //slider.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleBottomMargin;
-        }
-        else {
-            // place types section
-            // TODO: make cells in this section selectable
-            cell.textLabel?.text = placeTypes[indexPath.row]
-            cell.accessoryType = UITableViewCellAccessoryType.Checkmark
-        }
-        
-        return cell
+  
+  /*************************
+   **                     **
+   ** UITableView Methods **
+   **                     **
+   *************************/
+  
+  var placeTypes = ["Bar" : "bar", "Bakery" : "bakery", "Cafe" : "cafe",
+    "Casino" : "casino", "Convenience Store" : "convenience_store",
+    "Grocery Store / Supermarket" : "grocery_or_supermarket",
+    "Liquor Store" : "liquor_store", "Night Club" : "night_club",
+    "Restaurant" : "restaurant"]
+  
+  func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+    return 2;
+  }
+  
+  func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    // the following assumes there are only 2 sections
+    return section == 0 ? 1 : placeTypes.count
+  }
+  
+  func tableView(tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+    // the following assumes there are only 2 sections
+    return section == 0 ? "Search radius" : "Place type"
+  }
+  
+  func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+    // the following assumes there are only 2 sections
+    let cell = tableView.dequeueReusableCellWithIdentifier("tableCell", forIndexPath: indexPath)
+    
+    if indexPath.section == 0 {
+      // "search radius" slider section
+      let slider = UISlider()
+      
+      // TODO: fix images showing as black rectangle
+      cell.contentView.addSubview(slider)
+      let imgSize = CGSize(width: slider.bounds.size.height, height: slider.bounds.size.height)
+      let minImage = ATCUtils.scaleUIImageToSize(UIImage(named: "slider-minus")!, size: imgSize)
+      let maxImage = ATCUtils.scaleUIImageToSize(UIImage(named: "slider-plus")!, size: imgSize)
+      slider.minimumValueImage = minImage
+      slider.maximumValueImage = maxImage
+      
+      slider.bounds = CGRectMake(0, 0, cell.contentView.bounds.size.width - 10, slider.bounds.size.height)
+      slider.center = CGPointMake(CGRectGetMidX(cell.contentView.bounds), CGRectGetMidY(cell.contentView.bounds))
+      //slider.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleTopMargin | UIViewAutoresizingFlexibleBottomMargin;
     }
+    else {
+      // place types section
+      let filters = Array(placeTypes.keys)
+      cell.textLabel?.text = filters[indexPath.row]
+      
+      // TODO: make cells selected according to the configuration in MapView
+      if cell.selected {
+        cell.accessoryType = UITableViewCellAccessoryType.Checkmark
+      }
+      else {
+        cell.accessoryType = UITableViewCellAccessoryType.None
+      }
+    }
+    
+    return cell
+  }
+  
+  func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+    let cell = tableView.cellForRowAtIndexPath(indexPath)!
+    
+    cell.accessoryType = UITableViewCellAccessoryType.Checkmark
+    
+  }
+  
+  func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
+    let cell = tableView.cellForRowAtIndexPath(indexPath)!
+    
+    cell.accessoryType = UITableViewCellAccessoryType.None
+  }
 }
